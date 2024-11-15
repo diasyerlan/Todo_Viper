1c96acdcdfe4c89ab7de35594982d968d3d9a8e3
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2024-11-15 22:55:07 +0500

    Initial Commit

---
+++ TodoAppTests/TodoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppTests.swift
+//  TodoAppTests
+//
+//  Created by Dias Yerlan on 15.11.2024.
+//
+
+import Testing
+@testable import TodoApp
+
+struct TodoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
